2acaef0588fe2ddd555a448255a310f3010e191b
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-08-22 10:32:29 +0300

    RACT Tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,31 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => (
+  <div data-cy="TabsComponent">
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(t => (
+          <li
+            data-cy="Tab"
+            key={t.id}
+            className={classNames({ 'is-active': t.id === activeTabId })}
+          >
+            {t.id !== activeTabId ? (
+              <a
+                data-cy="TabLink"
+                href={`#${t.id}`} onClick={() => onTabSelected(t.id)}>
+                {t.title}
+              </a>
+            ) : (
+              <a
+                data-cy="TabLink"
+                href={`#${t.id}`}>
+                  {t.title}
+              </a>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+);
