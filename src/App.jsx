9780620f3509b7e672a8bbe5a97b17982c187c7d
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-08-23 18:19:01 +0300

    Move h1 into tabs

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,12 @@ export const tabs = [
 
 export const App = () => {
   const [activeTabId, setActiveTabId] = useState(tabs[0].id);
+  const activeTab = tabs.find(t => t.id === activeTabId) || tabs[0];
 
   return (
     <div className="section">
+      <h1 className="title">Selected tab is {activeTab.title}</h1>
+
       <Tabs
         tabs={tabs}
         activeTabId={activeTabId}
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -5,10 +5,9 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
 
   return (
     <div data-cy="TabsComponent">
-
       <div className="tabs is-boxed">
         <ul>
-          <h1 className="title">Selected tab is {activeTab.title}</h1>
+
           {tabs.map(t => (
             <li
               data-cy="Tab"
@@ -31,11 +30,10 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
             </li>
           ))}
         </ul>
-
       </div>
       <div className="block" data-cy="TabContent">
-          {activeTab.content}
-        </div>
+        {activeTab.content}
+      </div>
     </div>
   );
 };
